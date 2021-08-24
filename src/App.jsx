ba5125433c1ec3d2eb25abf9d1330f0b32863182
unknown <elaad2408@gmail.com>
2021-08-24 19:03:33 +0300

    add routhes and dynamic pages

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+import { Router, Switch, Route, Redirect } from "react-router";
+
+//components
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import ProductsPage from "./components/ProductsPage";
+import PageNotFound404 from "./components/PageNotFound404";
+
+function App() {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <header>
+        {/* need  to conect items to redux->database to take the about of product in shoppingcart   */}
+        <NavBar items={0} />
+      </header>
+
+      <main className="container-fluid flex-fill">
+        <Switch>
+          <Route path="/PageNotFound404" component={PageNotFound404} />
+
+          <Route path="/home" component={Home} />
+          <Route path="/ProductsPage" component={ProductsPage} />
+          <Route
+            path="/ProductsPage?category=clothing"
+            component={ProductsPage}
+          />
+          <Route
+            path="/ProductsPage?category=electronics"
+            component={ProductsPage}
+          />
+          <Route
+            path="/ProductsPage?category=contactus"
+            component={ProductsPage}
+          />
+          <Route
+            path="/ProductsPage?category=wishlist"
+            component={ProductsPage}
+          />
+          <Route
+            path="/ProductsPage?category=shoppingcart"
+            component={ProductsPage}
+          />
+
+          <Redirect to="/PageNotFound404" />
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
